d90d9bd567d149305e4e8756b5f6c1864ff20aa7
Michael Connolly <mpc63@uakron.edu>
2024-01-31 23:18:41 -0500

    Add advanced options dropdown

--- src/app/_components/create-poll.tsx
+++ src/app/_components/create-poll.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { PlusIcon } from "@heroicons/react/24/solid";
+import {
+  ChevronRightIcon,
+  ChevronDownIcon,
+  PlusIcon,
+} from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -10,6 +14,7 @@ export function CreatePoll() {
   const [title, setTitle] = useState("");
   const [optionInput, setOptionInput] = useState("");
   const [options, setOptions] = useState<string[]>([]);
+  const [showSettings, setShowSettings] = useState(false);
 
   const createPoll = api.poll.create.useMutation({
     onSuccess: () => {
@@ -36,7 +41,7 @@ export function CreatePoll() {
           e.preventDefault();
           createPoll.mutate({ title, options });
         }}
-        className="flex flex-col gap-2 rounded p-2 shadow-lg border border-gray-400 bg-white"
+        className="flex flex-col gap-2 rounded border border-gray-400 bg-white p-2 shadow-lg"
       >
         <div className="text-center">
           <h1 className="text-xl font-extrabold">create poll.</h1>
@@ -46,7 +51,7 @@ export function CreatePoll() {
           placeholder="Title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="w-full rounded px-4 py-2 text-black border border-gray-400 shadow-lg"
+          className="w-full rounded border border-gray-400 px-4 py-2 text-black shadow-lg"
         />
         <div className="flex gap-2">
           <input
@@ -54,17 +59,17 @@ export function CreatePoll() {
             placeholder="Option"
             value={optionInput}
             onChange={(e) => setOptionInput(e.target.value)}
-            className="w-full rounded px-4 py-2 text-black border border-gray-400 shadow-lg"
+            className="w-full rounded border border-gray-400 px-4 py-2 text-black shadow-lg"
           />
           <button
             type="button"
             onClick={handleOptionAddition}
-            className="rounded-full bg-green-400 px-6 py-2 font-semibold transition hover:scale-105 hover:bg-green-500 shadow-lg"
+            className="rounded-full bg-green-400 px-6 py-2 font-semibold shadow-lg transition hover:scale-105 hover:bg-green-500"
           >
-            <PlusIcon className="w-6 h-6 text-white" />
+            <PlusIcon className="h-6 w-6 text-white" />
           </button>
         </div>
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex flex-wrap gap-2">
           {options.map((option, index) => (
             <button
               type="button"
@@ -72,15 +77,48 @@ export function CreatePoll() {
               onClick={() => {
                 setOptions(options.filter((o) => o !== option));
               }}
-              className="rounded-full border border-gray-400 px-4 cursor-pointer select-none hover:bg-red-500"
+              className="cursor-pointer select-none rounded-full border border-gray-400 px-4 hover:bg-red-500"
             >
               {option}
             </button>
           ))}
         </div>
         <button
+          type="button"
+          onClick={() => {
+            setShowSettings(!showSettings);
+          }}
+        >
+          <div className="flex place-items-center gap-2">
+            <div className="flex">
+              {showSettings ? (
+                <ChevronDownIcon className="h-6 w-6 text-black" />
+              ) : (
+                <ChevronRightIcon className="h-6 w-6 text-black" />
+              )}
+              <p className="font-bold">Advanced</p>
+            </div>
+            <hr className="w-full rounded border border-black" />
+          </div>
+        </button>
+        <div
+          className={`flex place-items-center gap-2 ${showSettings ? "" : "hidden"}`}
+        >
+          <div className="flex place-items-center">
+            <p>Time:</p>
+            <select name="expires" id="expires">
+              <option value="1">1d</option>
+              <option value="7">7d</option>
+            </select>
+          </div>
+          <div className="flex place-items-center gap-2">
+            <p>Secure:</p>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <button
           type="submit"
-          className="rounded-full bg-black/10 px-10 py-3 font-semibold transition hover:bg-black/20 border border-gray-400 shadow-lg"
+          className="rounded-full border border-gray-400 bg-black/10 px-10 py-3 font-semibold shadow-lg transition hover:bg-black/20"
           disabled={createPoll.isLoading}
         >
           {createPoll.isLoading ? "Submitting..." : "Submit"}
